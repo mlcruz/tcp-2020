56a2a98c2596329faadff93ee196981ab003b518
Matheus Cruz <mlcruz@inf.ufrgs.br>
2020-09-12 16:42:52 -0300

    working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import SoundGeneratorButton from "./components/SoundGeneratorButton";
 import { parseNotesFromText } from "./utils/parseTextInput";
 
 // Componente principal da aplicação.
+// Utilizamos aqui um componente funcional em vez de um orientado a objetos
+// Componentes orientados a objeto são "legacy", e utilizaremos no resto do trabalho só para
+// conformar com a necessidade do trabalho ser orientado a objetos
+// Um componente funcionar retornando é o equivalente de uma chamada do render() em um compoennete orientado a objetos
+
 function App() {
-  console.log(parseNotesFromText("A1A2A3A3B#4"));
+  // Aqui criamos um estado para o campo de texto, iniciando vazio. A ideia é que o input vai modificar
+  // esse campo.
+  // O texto docampo então é passado como parametro para o componente gerador de som
+
+  const [textInput, setTextInput] = useState(
+    "A1A2A3A4B4B3B2B1C1C2C3C4D4D3D2D1"
+  );
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="container">
-          <div style={{ marginTop: "10%" }}>
-            <div
-              className="row justify-content-center align-items-center"
-              style={{ marginTop: "auto", marginBottom: "auto" }}
-            >
-              <div className={"col-sm-6"}>
-                <input type="text" className="form-control" />
-              </div>
-              <div className={"colm-6"}>
-                <SoundGeneratorButton
-                  input={"A1C2C3D4G5"}
-                ></SoundGeneratorButton>
-              </div>
+          <div style={{ marginTop: "10%" }}></div>
+          <div className="row justify-content-center align-items-center">
+            <div className={"alert alert-primary"}>
+              <p>
+                Uma nota é um caractere seguido de um numero, com um possivel
+                sustenido no meio. Sempre terminador por numero. (Vamos mudar
+                isso depois).
+              </p>
+              <p>
+                O caractere representa (+sustenido) a 'Nota' e o numero a
+                oitava. Ex: A8, B6, C#2
+              </p>
+
+              <p>Notas tocam a cada 0.5 segundo.</p>
+            </div>
+          </div>
+          <div className="row justify-content-center align-items-center">
+            <div className={"col-sm-6"}>
+              <input
+                type="text"
+                className="form-control"
+                value={textInput}
+                onChange={(e) => {
+                  setTextInput(e.target.value);
+                }}
+              />
+            </div>
+            <div className={"colm-6"}>
+              <SoundGeneratorButton input={textInput}></SoundGeneratorButton>
             </div>
           </div>
         </div>
