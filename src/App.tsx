477529c1a552d1047c287d84fe519f72290e0940
martim <martimkun@gmail.com>
2020-11-15 22:23:50 -0300

    atualizacao texto instrucoes

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,39 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="container">
-          <div style={{ marginTop: "10%" }}>
+          <div style={{ marginTop: "5%" }}>
             <div className="row justify-content-center align-items-center">
-              <div className={"alert alert-primary"}>
-                <p>Insira o texto para gerar o som na caixa abaixo</p>
-                <strong>Lista de comandos:</strong>
+              <div className={"alert alert-primary"} style = {{fontSize: 14 , width: 800}}>
+                <strong style = {{fontSize: 16}}>Instruções de uso:</strong>
+                <p style = {{paddingLeft: 10}}>Insira o texto na caixa abaixo e/ou arquivo para gerar um som. Cada caractere corresponde a um tipo de nota ou ação, abaixo seguem as instruções:</p>
+                
+                <strong style = {{marginTop: 15}}>- Para tocar notas musicais, digite:</strong>
+                <p style = {{paddingLeft: 20}}>As letras: A,B,C,D,E,F ou G</p>
+
+                <strong style = {{marginTop: 35}}>- Para tocar o instrumento, digite:</strong>
+                <p style = {{paddingLeft: 20}}>- '!' para trocar para Agogo;</p>
+                <p style = {{paddingLeft: 20}}>- 'O,o,U,u,I,i' para trocar para Harpsichord;</p>
+                <p style = {{paddingLeft: 20}}>- 'enter' para trocar para Sinos Tubulares;</p>
+                <p style = {{paddingLeft: 20}}>- ',' para trocar para Orgão de Igreja;</p>
+                <p style = {{paddingLeft: 20}}>- Qualquer número para trocar para outro instrumento aleatório;</p>
+
+                <strong style = {{marginTop: 35}}>- Para dobrar o volume de reprodução, digite:</strong>
+                <p style = {{paddingLeft: 20}}>- O caractere 'espaço' (caso não seja possível aumentar, o volume irá voltar ao normal).</p>
+
+                <strong style = {{marginTop: 35}}>- Para aumentar uma oitava da nota, digite:</strong>
+                <p style = {{paddingLeft: 20}}>- O caractere '?'</p>
+
+                <strong style = {{marginTop: 35}}>- Para repetir a nota anterior digite (caso o caractere anterior não seja uma nota, será feita uma pausa ou silêncio):</strong>
+                <p style = {{paddingLeft: 20}}> - As letras: a,b,c,d,e,f ou g</p>
+                <p style = {{paddingLeft: 20}}> - Qualquer consoante que não esteja presente no conjunto de notas;</p>
+                <p style = {{paddingLeft: 20}}> - Qualquer outro caractere não referenciado nos outros tópicos.</p>
+                {/* <strong>Lista de comandos:</strong>
                 <ul>
                   <li>
                     Uma nota é um caractere seguido de A até G (maiusculo).
                   </li>
                   <li>I Troca o instrumento</li>
-                </ul>
+                </ul> */}
               </div>
             </div>
             <div className="row">
